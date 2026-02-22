a0fc0aa0554f5176e7f502c9ccf6b2c0dad4904d
nataliechipashvili-lab <nataliechipashvili@gmail.com>
2026-02-22 13:34:57 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+function App() {
+//js
+  const username = 'Nata'
+
+
+  return (
+   //html
+    <div>
+      <header>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </header>
+
+      <h1>hello {username} </h1>
+
+      <p className='blueText'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, dolores?</p>
+      
+      <button>Click</button>
+    </div>
+  )
+}
+
+export default App
