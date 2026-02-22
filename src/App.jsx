0a9052524b11340f5981974dc735178dd489ac2a
nataliechipashvili-lab <nataliechipashvili@gmail.com>
2026-02-22 13:43:46 +0400

    change title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
 //js
-  const username = 'Nata'
+  const username = 'Natali'
 
 
   return (
@@ -20,7 +20,7 @@ function App() {
       <h1>hello {username} </h1>
 
       <p className='blueText'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, dolores?</p>
-      
+
       <button>Click</button>
     </div>
   )
